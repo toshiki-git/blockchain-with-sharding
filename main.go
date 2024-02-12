3dd99f536c46c279fa60ea93698876a6b95845bc
Toshiki Takakubo <219tosiki@gmail.com>
2024-02-12 20:21:25 +0900

    Refactor node creation and add block sending functionality

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 
 func main() {
 	manager := p2p.NewNodeManager()
-	node1 := manager.CreateNode("/echo/1.0.0")
-	node2 := manager.CreateNode("/echo/1.0.0")
+	node1 := manager.CreateNode()
+	node2 := manager.CreateNode()
 
 	manager.ConnectNode(node1, node2)
 
 	manager.ListNodes()
 
-	manager.SendMessage(node1, node2, "/echo/1.0.0")
+	//manager.SendMessage(node1, node2, "/message/1.0.0")
+	manager.SendBlock(node1, node2, "/block/1.0.0")
 
 	select {}
 }
